5bd80e1d17ce5b575f382c07df5491192dd2c551
Pierre Flande <pflande@matchbox.fr>
2020-09-29 17:11:31 +0200

    deploy test

---
+++ build/precache-manifest.a314bb01694c094518fc73c191e3a266.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c141c3fa78ca1d1f4a51a5766440028d",
+    "url": "/lyrics-finder/index.html"
+  },
+  {
+    "revision": "5841401ba3ab2a5fba54",
+    "url": "/lyrics-finder/static/css/main.f2ecc68a.chunk.css"
+  },
+  {
+    "revision": "b17835341203e5f19972",
+    "url": "/lyrics-finder/static/js/2.f878fcd0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lyrics-finder/static/js/2.f878fcd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5841401ba3ab2a5fba54",
+    "url": "/lyrics-finder/static/js/main.12428976.chunk.js"
+  },
+  {
+    "revision": "f8f1577f707650a872ab",
+    "url": "/lyrics-finder/static/js/runtime-main.4daa7b91.js"
+  },
+  {
+    "revision": "8ecda6511c166e9665d80979a4db4e43",
+    "url": "/lyrics-finder/static/media/Scratched-Letters.8ecda651.otf"
+  },
+  {
+    "revision": "9e66429ad2c9232a5e0661e945dbb3c8",
+    "url": "/lyrics-finder/static/media/WorkSans-Light.9e66429a.otf"
+  },
+  {
+    "revision": "9589dc995222e6c5d5b67bf789ef5ce6",
+    "url": "/lyrics-finder/static/media/WorkSans-Regular.9589dc99.otf"
+  }
+]);
