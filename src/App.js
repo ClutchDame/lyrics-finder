124b6fafb5993b4d3ee36ea6f413de85520f0929
Pierre Flande <pflande@matchbox.fr>
2020-09-29 12:18:02 +0200

    functional app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,111 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from 'react'
+import { v4 as uuidv4 } from 'uuid'
+import './styles.scss'
 
-function App() {
+export default function App() {
+  const ref = useRef(null)
+  const [lyrics, setLyrics] = useState()
+  const [artist, setArtist] = useState(null)
+  const [chosenArtist, setChosenArtist] = useState()
+  const [searchResults, setSearchResults] = useState([])
+  const errorMessage = 'Sorry we could not find these lyrics'
+
+  //Replace space with accepted characters
+  const sanitizeStr = str => str.replace(/\s/, '%20')
+
+  const getSuggestions = async search => {
+    sanitizeStr(search)
+    const SEARCH_API = `https://api.lyrics.ovh/suggest/${search}`
+
+    const response = await fetch(SEARCH_API)
+    const results = await response.json()
+
+    return results
+  }
+
+  const getLyrics = async (artist, title) => {
+    const [cleanArtist, cleanTitle] = [sanitizeStr(artist), sanitizeStr(title)]
+    const LYRICS_API = `https://api.lyrics.ovh/v1/${cleanArtist}/${cleanTitle}`
+
+    const response = await fetch(LYRICS_API)
+    const lyrics = await response.json()
+
+    return lyrics
+  }
+
+  const handleChange = e => {
+    const search = e.target.value
+    // when we have at least 3 characters typed in by user, start searching
+    if (search.length > 2) {
+      getSuggestions(search).then(({ error, data: results }) => {
+        results && setSearchResults(results)
+        error && console.log('boom error fetching suggestions -> ', error)
+      })
+    } else setSearchResults([])
+  }
+
+  const handleSuggestionClick = result => {
+    setChosenArtist(
+      `${result.artist.name} - ${
+        result.title_short ? result.title_short : result.title
+      }`
+    )
+    getLyrics(
+      result.artist.name,
+      // TODO: Test which is better btw title and title_short
+      result.title_short ? result.title_short : result.title
+    ).then(({ lyrics, error }) => {
+      lyrics ? setLyrics(lyrics) : setLyrics(errorMessage)
+      error && console.log('boom error fetching lyrics ->', error)
+      setSearchResults([])
+    })
+    result.artist && setArtist(result.artist)
+  }
+
+  const handleKeyDown = (e, index, result) => {
+    e.key === 'Enter' && handleSuggestionClick(result)
+    e.key === 'Tab' &&
+      index + 1 === searchResults.length &&
+      ref.current.parentNode.firstChild.focus()
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>Lyrics finder</h1>
+      <section>
+        <input
+          type="text"
+          name="search"
+          placeholder="Artist / Song"
+          onChange={e => handleChange(e)}
+          value={chosenArtist}
+        />
+        <ul tabIndex="-1" aria-expanded={!!searchResults?.length > 0}>
+          {searchResults?.map((result, index) => {
+            return (
+              <li
+                ref={ref}
+                key={uuidv4()}
+                tabIndex={0}
+                onClick={e => handleSuggestionClick(result)}
+                onKeyDown={e => handleKeyDown(e, index, result)}
+              >
+                {result.artist.name} -{' '}
+                {result.title_short ? result.title_short : result.title}
+              </li>
+            )
+          })}
+        </ul>
+      </section>
+      {artist && (
+        <img
+          alt="artist poster"
+          src={artist.picture}
+          srcset={`${artist.picture_medium} 250w, ${artist.picture_big} 500w`}
+          sizes="20vmin"
+        />
+      )}
+      <pre>{lyrics}</pre>
+    </>
+  )
 }
-
-export default App;
